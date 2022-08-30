d463f932336835df01bbe7a8ea42bbec3a7e066b
Kohei Araya <kohi218ponmiso@gmail.com>
2022-08-30 18:35:13 +0900

    ActionBarを非表示

--- app/src/main/java/kohei/araya/newsapiandroid/MainActivity.kt
+++ app/src/main/java/kohei/araya/newsapiandroid/MainActivity.kt
@@ -3,8 +3,6 @@ package kohei.araya.newsapiandroid
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,14 +22,6 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_feed, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
